91103a83be7f4ce6842e823ffb700eecbc773d90
Sushmmita-Rudra <S537491@NWMISSOURI.EDU>
2020-11-08 18:19:56 -0600

    Added ui changes of order history.

--- Codes/Big-Kart/src/app/shopping/components/my-orders/my-orders.component.ts
+++ Codes/Big-Kart/src/app/shopping/components/my-orders/my-orders.component.ts
@@ -1,9 +1,8 @@
 import { AuthService } from "shared/services/auth.service";
 import { OrderService } from "../../../shared/services/order.service";
+
 import { Component, OnInit } from "@angular/core";
 import "rxjs/add/operator/switchMap";
-import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
-
 @Component({
   selector: "app-my-orders",
   templateUrl: "./my-orders.component.html",
@@ -11,36 +10,13 @@ import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
 })
 export class MyOrdersComponent {
   orders$;
-  closeResult = "";
 
   constructor(
     private authService: AuthService,
-    private orderService: OrderService,
-    private modalService: NgbModal
+    private orderService: OrderService
   ) {
     this.orders$ = authService.user$.switchMap((u) =>
       orderService.getOrdersByUser(u.uid)
     );
-  }
-
-  open(content) {
-    this.modalService.open(content).result.then(
-      (result) => {
-        this.closeResult = `Closed with: ${result}`;
-      },
-      (reason) => {
-        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-      }
-    );
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return "by pressing ESC";
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return "by clicking on a backdrop";
-    } else {
-      return `with: ${reason}`;
-    }
   }
 }
